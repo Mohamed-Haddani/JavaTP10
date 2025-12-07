79dc535478d057b1a02cf0c34daa593a678265ba
Mohamed Haddani <mohamed.haddani.2003@gmail.com>
2025-12-07 10:14:23 +0100

    Add files via upload

---
+++ tp10Ex1/Triplet.java
@@ -0,0 +1,37 @@
+package tp10Ex1;
+
+public class Triplet<T> {
+    private final T premier;
+    private final T second;
+    private final T troisieme;
+
+    /** Constructeur principal */
+    public Triplet(T premier, T second, T troisieme) {
+        this.premier    = premier;
+        this.second     = second;
+        this.troisieme  = troisieme;
+    }
+
+    /** Accesseur du premier élément */
+    public T getPremier() {
+        return premier;
+    }
+
+    /** Accesseur du second élément */
+    public T getSecond() {
+        return second;
+    }
+
+    /** Accesseur du troisième élément */
+    public T getTroisieme() {
+        return troisieme;
+    }
+
+    /** Affiche les trois éléments */
+    public void affiche() {
+        System.out.println("Triplet : ["
+            + premier + ", "
+            + second + ", "
+            + troisieme + "]");
+    }
+}
